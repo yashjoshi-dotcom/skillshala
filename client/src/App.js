d88544ded07c7545e76271291af2986cb5475953
Simarjeet Singh <sjthecoder@gmail.com>
2022-09-17 15:02:43 +0530

    removed app.css file

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,17 @@
 // import logo from './logo.svg';
-import React from 'react';
+import React from "react";
 import { Route, Routes } from "react-router-dom";
-import './App.css';
 import Landingpage from "./pages/landingpage";
 
 function App() {
-  return (
-    <div className="App">
-      <Routes>
-      <Route path="/" element={<Landingpage />} />
-      </Routes>
-      hello
-    </div>
-  );
+	return (
+		<div className="App">
+			<Routes>
+				<Route path="/" element={<Landingpage />} />
+			</Routes>
+			hello
+		</div>
+	);
 }
 
 export default App;
