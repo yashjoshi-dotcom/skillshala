cba100dc63ca9762dc9cd3a92c5e5e66fe8d7120
Simarjeet Singh <sjthecoder@gmail.com>
2022-09-17 20:21:21 +0530

    current user check add for signup change

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { setDoc, doc } from "firebase/firestore";
 import { auth, db } from "../firebase-config";
+import { useAuth } from "../context/AuthContext";
 
 function SignUp(props) {
 	// Local States
 	const [loading, setLoading] = useState(false);
 	const [error, setError] = useState(false);
+
+	// Variables
+	const { currentUser, setCurrentUser } = useAuth();
+	const navigate = useNavigate();
+	console.log(currentUser);
 
 	// Local form refs
 	const fullNameRef = useRef();
@@ -48,7 +55,9 @@ function SignUp(props) {
 									fullname: fullName,
 									gender: gender,
 								});
+								setCurrentUser(cred);
 								setLoading(false);
+								navigate("/");
 							} catch (error) {
 								setError(error.message);
 								setLoading(false);
@@ -80,80 +89,83 @@ function SignUp(props) {
 			error
 		);
 	}
-
-	return (
-		<div className="flex h-screen w-full items-center justify-center bg-slate-700">
-			<div className="h-5/6 w-1/3 rounded-xl  bg-black bg-opacity-30 px-8 py-4 shadow-slate-900 ">
-				<h1 className="text-center text-2xl font-bold italic text-white">
-					Sign Up
-				</h1>
-				<form
-					onSubmit={onSubmitHandler}
-					className="flex h-full flex-col justify-evenly"
-				>
-					<div className={containerClass}>
-						<span className={spanClass}>Full Name</span>
-						<input
-							ref={fullNameRef}
-							type="text"
-							placeholder="Eg: Yuvraj"
-							className={inputClasses}
-							required
-						></input>
-					</div>
-					<div className={containerClass}>
-						<span className={spanClass}>Email</span>
-						<input
-							ref={emailRef}
-							type="email"
-							placeholder="Eg: yuvi@gmail.com"
-							className={inputClasses}
-							required
-						></input>
-					</div>
-					<div className={containerClass}>
-						<span className={spanClass}>Gender</span>
-						<select
-							ref={genderRef}
-							name="Gender"
-							id="gender"
-							className={inputClasses}
-							required
-						>
-							<option value="Male">Male</option>
-							<option value="Female">Female</option>
-						</select>
-					</div>
-					<div className={containerClass}>
-						<span className={spanClass}>Password</span>
-						<input
-							ref={passwordRef}
-							type="password"
-							className={inputClasses}
-							required
-						></input>
-					</div>
-					<div className={containerClass}>
-						<span className={spanClass}>Confirm Password</span>
-						<input
-							ref={confirmPasswordRef}
-							type="password"
-							className={inputClasses}
-							required
-						></input>
-					</div>
-					<button
-						type="submit"
-						className="self-end rounded bg-teal-500 px-6 py-2 text-white  hover:bg-teal-700 disabled:bg-pink-500"
-						disabled={loading}
-					>
+	if (currentUser) {
+		return navigate("/");
+	} else {
+		return (
+			<div className="flex h-screen w-full items-center justify-center bg-slate-700">
+				<div className="h-5/6 w-1/3 rounded-xl  bg-black bg-opacity-30 px-8 py-4 shadow-slate-900 ">
+					<h1 className="text-center text-2xl font-bold italic text-white">
 						Sign Up
-					</button>
-					{error ? <p className="text-rose-500">{error}</p> : null}
-				</form>
+					</h1>
+					<form
+						onSubmit={onSubmitHandler}
+						className="flex h-full flex-col justify-evenly"
+					>
+						<div className={containerClass}>
+							<span className={spanClass}>Full Name</span>
+							<input
+								ref={fullNameRef}
+								type="text"
+								placeholder="Eg: Yuvraj"
+								className={inputClasses}
+								required
+							></input>
+						</div>
+						<div className={containerClass}>
+							<span className={spanClass}>Email</span>
+							<input
+								ref={emailRef}
+								type="email"
+								placeholder="Eg: yuvi@gmail.com"
+								className={inputClasses}
+								required
+							></input>
+						</div>
+						<div className={containerClass}>
+							<span className={spanClass}>Gender</span>
+							<select
+								ref={genderRef}
+								name="Gender"
+								id="gender"
+								className={inputClasses}
+								required
+							>
+								<option value="Male">Male</option>
+								<option value="Female">Female</option>
+							</select>
+						</div>
+						<div className={containerClass}>
+							<span className={spanClass}>Password</span>
+							<input
+								ref={passwordRef}
+								type="password"
+								className={inputClasses}
+								required
+							></input>
+						</div>
+						<div className={containerClass}>
+							<span className={spanClass}>Confirm Password</span>
+							<input
+								ref={confirmPasswordRef}
+								type="password"
+								className={inputClasses}
+								required
+							></input>
+						</div>
+						<button
+							type="submit"
+							className="self-end rounded bg-teal-500 px-6 py-2 text-white  hover:bg-teal-700 disabled:bg-pink-500"
+							disabled={loading}
+						>
+							Sign Up
+						</button>
+						{error ? <p className="text-rose-500">{error}</p> : null}
+					</form>
+				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
 
 export default SignUp;
