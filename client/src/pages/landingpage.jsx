aae62b419594cecdc932044d70057d15171c5eb7
Tanush15 <106920983+Tanush15@users.noreply.github.com>
2022-09-17 14:25:09 +0530

    landing page

---
+++ client/src/pages/landingpage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Landingpage = () => {
+    return (
+        <>
+           {/* <h1>Hello</h1> */}
+           <div className="bg-black h-[100vh]">
+           <div className="">
+            
+           </div>
+           </div>
+        </>
+      
+    )
+}
+export default Landingpage;
