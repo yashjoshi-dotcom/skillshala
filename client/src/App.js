7b2cf2ee590bb247126533d823dfd8a8cd00f912
yashjoshi-dotcom <77540753+yashjoshi-dotcom@users.noreply.github.com>
2022-09-17 20:32:35 +0530

    Video Call

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import Landingpage from "./pages/landingpage";
 import SignUp from "./pages/SignUp";
+import Video  from "./pages/video";
 import Upload from "./pages/Upload";
 import Home from "./pages/home";
 import Navabr from "./pages/navbar";
@@ -15,6 +16,7 @@ function App() {
 				<Navabr />
 				<Routes>
 					<Route path="/" element={<Landingpage />} />
+					<Route path="/video" element={<Video />} />
 					<Route path="/signup" element={<SignUp />} />
 					<Route path="/home" element={<Home />} />
 					<Route path="/upload" element={<Upload />} />
