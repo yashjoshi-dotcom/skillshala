0d0c36fc82657bf5b908c04602103b28b1c59f15
yashjoshi-dotcom <77540753+yashjoshi-dotcom@users.noreply.github.com>
2022-09-17 14:33:10 +0530

    fixed router

--- client/src/pages/landingpage.js
+++ client/src/pages/landingpage.js
@@ -6,7 +6,7 @@ const Landingpage = () => {
            {/* <h1>Hello</h1> */}
            <div className="bg-black h-[100vh]">
            <div className="">
-            
+            hi77----
            </div>
            </div>
         </>
