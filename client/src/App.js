189eaaaaac9e900fb4a8065db9055091f67c26b5
Simarjeet Singh <sjthecoder@gmail.com>
2022-09-17 15:05:15 +0530

    added signup page

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Landingpage from "./pages/landingpage";
+import SignUp from "./pages/SignUp";
 
 function App() {
 	return (
 		<div className="App">
 			<Routes>
 				<Route path="/" element={<Landingpage />} />
+				<Route path="/signup" element={<SignUp />} />
 			</Routes>
-			hello
 		</div>
 	);
 }
