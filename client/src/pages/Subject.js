b739f5598b5cf7eae15346a4e4d837dde26a0dc3
Tanush15 <106920983+Tanush15@users.noreply.github.com>
2022-09-18 01:17:12 +0530

    Update Subject.js

--- client/src/pages/Subject.js
+++ client/src/pages/Subject.js
@@ -3,13 +3,173 @@ import css from "./css.css";
 const subject = () => {
     return (
         <>
-        <div className=" bg-black">
-            <div className=" h-[40vh] bg-[url('https://source.unsplash.com/1600x400/?study')] bg-cover ">
-            <div className=" text-center text-5xl font-extrabold italic text-gray-800"> Sub Topics of Computer Science</div>
+            <div className="">
+                <div className=" bg-gradient-to-b from-gray-700 via-gray-900 to-black   bg-cover font-bold text-5xl italic py-[7%] flex justify-center align-middle text-gray-300"> Computer Science Topics </div>
+                <div className=" bg-black min-h-[53vh] ">
+                    <div className=" text-white pl-5 pb-5 text-2xl font-semibold ">Data Structures and Algorithms</div>
+                    <div className="flex h-fit overflow-x-auto">
+                        {/* <div className=""></div> */}
+                        <div class="py-5 px-3 ">
+                            <div class="shadow-lg group container  rounded-md bg-white  max-w-sm flex justify-center items-center  mx-auto content-div">
+                                <div>
+                                    <div class=" h-[10vh] w-[28vw]  image-cover rounded-t-md " >
+                                        {/* <div class="p-2 m-4 w-16 h-16 text-center bg-gray-700 rounded-full text-white float-right fd-cl group-hover:opacity-25">
+                                        <span class="text-base tracking-wide  font-bold border-b border-white font-sans"> 12</span>
+                                        <span class="text-xs tracking-wide font-bold uppercase block font-sans">April</span>
+                                    </div> */}
+                                    </div>
+                                    {/* <div class="py-8 px-4 bg-white  rounded-b-md fd-cl group-hover:opacity-25">
+                                    <span class="block text-lg text-gray-800 font-bold tracking-wide">Book a flight</span>
+                                    <span class="block text-gray-600 text-sm">Vivamus ac ligula sit amet erat luctus laoreet ac quis ligula. Donec bibendum faucibus purus eget cursus. Proin enim ante, scelerisque vel sem sit amet, ultrices mollis risus. Praesent justo felis, ullamcorper a cursus sed, condimentum at dui.
+                                    </span>
+                                </div> */}
+                                </div>
+                                <div class="absolute opacity-0 fd-sh group-hover:opacity-100">
+                                    <span class="text-3xl font-bold text-white tracking-wider leading-relaxed font-sans">Teacher's Name</span>
+                                    <div class="text-xl text-center font-bold text-white tracking-wider leading-relaxed font-sans">Topic Name</div>
+                                    <div class="pt-8 text-center">
+                                        <button class="text-center rounded-lg p-4 bg-white  text-gray-700 font-bold text-lg">Join Lecture</button>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div class="py-5 px-3 ">
+                            <div class="shadow-lg group container  rounded-md bg-white  max-w-sm flex justify-center items-center  mx-auto content-div">
+                                <div>
+                                    <div class=" h-[10vh] w-[28vw]  image-cover rounded-t-md " >
+                                        {/* <div class="p-2 m-4 w-16 h-16 text-center bg-gray-700 rounded-full text-white float-right fd-cl group-hover:opacity-25">
+                                        <span class="text-base tracking-wide  font-bold border-b border-white font-sans"> 12</span>
+                                        <span class="text-xs tracking-wide font-bold uppercase block font-sans">April</span>
+                                    </div> */}
+                                    </div>
+                                    {/* <div class="py-8 px-4 bg-white  rounded-b-md fd-cl group-hover:opacity-25">
+                                    <span class="block text-lg text-gray-800 font-bold tracking-wide">Book a flight</span>
+                                    <span class="block text-gray-600 text-sm">Vivamus ac ligula sit amet erat luctus laoreet ac quis ligula. Donec bibendum faucibus purus eget cursus. Proin enim ante, scelerisque vel sem sit amet, ultrices mollis risus. Praesent justo felis, ullamcorper a cursus sed, condimentum at dui.
+                                    </span>
+                                </div> */}
+                                </div>
+                                <div class="absolute opacity-0 fd-sh group-hover:opacity-100">
+                                    <span class="text-3xl font-bold text-white tracking-wider leading-relaxed font-sans">Teacher's Name</span>
+                                    <div class="text-xl text-center font-bold text-white tracking-wider leading-relaxed font-sans">Topic Name</div>
+                                    <div class="pt-8 text-center">
+                                        <button class="text-center rounded-lg p-4 bg-white  text-gray-700 font-bold text-lg">Join Lecture</button>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div class="py-5 px-3 ">
+                            <div class="shadow-lg group container  rounded-md bg-white  max-w-sm flex justify-center items-center  mx-auto content-div">
+                                <div>
+                                    <div class=" h-[10vh] w-[28vw]  image-cover rounded-t-md " >
+                                        {/* <div class="p-2 m-4 w-16 h-16 text-center bg-gray-700 rounded-full text-white float-right fd-cl group-hover:opacity-25">
+                                        <span class="text-base tracking-wide  font-bold border-b border-white font-sans"> 12</span>
+                                        <span class="text-xs tracking-wide font-bold uppercase block font-sans">April</span>
+                                    </div> */}
+                                    </div>
+                                    {/* <div class="py-8 px-4 bg-white  rounded-b-md fd-cl group-hover:opacity-25">
+                                    <span class="block text-lg text-gray-800 font-bold tracking-wide">Book a flight</span>
+                                    <span class="block text-gray-600 text-sm">Vivamus ac ligula sit amet erat luctus laoreet ac quis ligula. Donec bibendum faucibus purus eget cursus. Proin enim ante, scelerisque vel sem sit amet, ultrices mollis risus. Praesent justo felis, ullamcorper a cursus sed, condimentum at dui.
+                                    </span>
+                                </div> */}
+                                </div>
+                                <div class="absolute opacity-0 fd-sh group-hover:opacity-100">
+                                    <span class="text-3xl font-bold text-white tracking-wider leading-relaxed font-sans">Teacher's Name</span>
+                                    <div class="text-xl text-center font-bold text-white tracking-wider leading-relaxed font-sans">Topic Name</div>
+                                    <div class="pt-8 text-center">
+                                        <button class="text-center rounded-lg p-4 bg-white  text-gray-700 font-bold text-lg">Join Lecture</button>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div class="py-5 px-3 ">
+                            <div class="shadow-lg group container  rounded-md bg-white  max-w-sm flex justify-center items-center  mx-auto content-div">
+                                <div>
+                                    <div class=" h-[10vh] w-[28vw]  image-cover rounded-t-md " >
+                                        {/* <div class="p-2 m-4 w-16 h-16 text-center bg-gray-700 rounded-full text-white float-right fd-cl group-hover:opacity-25">
+                                        <span class="text-base tracking-wide  font-bold border-b border-white font-sans"> 12</span>
+                                        <span class="text-xs tracking-wide font-bold uppercase block font-sans">April</span>
+                                    </div> */}
+                                    </div>
+                                    {/* <div class="py-8 px-4 bg-white  rounded-b-md fd-cl group-hover:opacity-25">
+                                    <span class="block text-lg text-gray-800 font-bold tracking-wide">Book a flight</span>
+                                    <span class="block text-gray-600 text-sm">Vivamus ac ligula sit amet erat luctus laoreet ac quis ligula. Donec bibendum faucibus purus eget cursus. Proin enim ante, scelerisque vel sem sit amet, ultrices mollis risus. Praesent justo felis, ullamcorper a cursus sed, condimentum at dui.
+                                    </span>
+                                </div> */}
+                                </div>
+                                <div class="absolute opacity-0 fd-sh group-hover:opacity-100">
+                                    <span class="text-3xl font-bold text-white tracking-wider leading-relaxed font-sans">Teacher's Name</span>
+                                    <div class="text-xl text-center font-bold text-white tracking-wider leading-relaxed font-sans">Topic Name</div>
+                                    <div class="pt-8 text-center">
+                                        <button class="text-center rounded-lg p-4 bg-white  text-gray-700 font-bold text-lg">Join Lecture</button>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div class="py-5 px-3 ">
+                            <div class="shadow-lg group container  rounded-md bg-white  max-w-sm flex justify-center items-center  mx-auto content-div">
+                                <div>
+                                    <div class=" h-[10vh] w-[28vw]  image-cover rounded-t-md " >
+                                        {/* <div class="p-2 m-4 w-16 h-16 text-center bg-gray-700 rounded-full text-white float-right fd-cl group-hover:opacity-25">
+                                        <span class="text-base tracking-wide  font-bold border-b border-white font-sans"> 12</span>
+                                        <span class="text-xs tracking-wide font-bold uppercase block font-sans">April</span>
+                                    </div> */}
+                                    </div>
+                                    {/* <div class="py-8 px-4 bg-white  rounded-b-md fd-cl group-hover:opacity-25">
+                                    <span class="block text-lg text-gray-800 font-bold tracking-wide">Book a flight</span>
+                                    <span class="block text-gray-600 text-sm">Vivamus ac ligula sit amet erat luctus laoreet ac quis ligula. Donec bibendum faucibus purus eget cursus. Proin enim ante, scelerisque vel sem sit amet, ultrices mollis risus. Praesent justo felis, ullamcorper a cursus sed, condimentum at dui.
+                                    </span>
+                                </div> */}
+                                </div>
+                                <div class="absolute opacity-0 fd-sh group-hover:opacity-100">
+                                    <span class="text-3xl font-bold text-white tracking-wider leading-relaxed font-sans">Teacher's Name</span>
+                                    <div class="text-xl text-center font-bold text-white tracking-wider leading-relaxed font-sans">Topic Name</div>
+                                    <div class="pt-8 text-center">
+                                        <button class="text-center rounded-lg p-4 bg-white  text-gray-700 font-bold text-lg">Join Lecture</button>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+
+
+                        <div class="py-5 px-3 ">
+                            <div class="shadow-lg group container  rounded-md bg-white  max-w-sm flex justify-center items-center  mx-auto content-div">
+                                <div>
+                                    <div class=" h-[10vh] w-[28vw]  image-cover rounded-t-md " >
+                                        {/* <div class="p-2 m-4 w-16 h-16 text-center bg-gray-700 rounded-full text-white float-right fd-cl group-hover:opacity-25">
+                                        <span class="text-base tracking-wide  font-bold border-b border-white font-sans"> 12</span>
+                                        <span class="text-xs tracking-wide font-bold uppercase block font-sans">April</span>
+                                    </div> */}
+                                    </div>
+                                    {/* <div class="py-8 px-4 bg-white  rounded-b-md fd-cl group-hover:opacity-25">
+                                    <span class="block text-lg text-gray-800 font-bold tracking-wide">Book a flight</span>
+                                    <span class="block text-gray-600 text-sm">Vivamus ac ligula sit amet erat luctus laoreet ac quis ligula. Donec bibendum faucibus purus eget cursus. Proin enim ante, scelerisque vel sem sit amet, ultrices mollis risus. Praesent justo felis, ullamcorper a cursus sed, condimentum at dui.
+                                    </span>
+                                </div> */}
+                                </div>
+                                <div class="absolute opacity-0 fd-sh group-hover:opacity-100">
+                                    <span class="text-3xl font-bold text-white tracking-wider leading-relaxed font-sans">Teacher's Name</span>
+                                    <div class="text-xl text-center font-bold text-white tracking-wider leading-relaxed font-sans">Topic Name</div>
+                                    <div class="pt-8 text-center">
+                                        <button class="text-center rounded-lg p-4 bg-white  text-gray-700 font-bold text-lg">Join Lecture</button>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
             </div>
-
-        </div>
         </>
-    )}
+    )
+}
 
-    export default subject;
+export default subject;
