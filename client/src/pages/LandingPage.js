ed341f60dde91e66e9ade763e5324af3b3c758a6
Simarjeet Singh <sjthecoder@gmail.com>
2022-09-17 21:44:36 +0530

    updated file names and functions

---
+++ client/src/pages/LandingPage.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Flowbite } from "flowbite-react";
+import { Carousel } from "flowbite-react";
+
+const LandingPage = () => {
+	return (
+		<>
+			{/* <h1>Hello</h1> */}
+
+			<div className="h-[90.6vh] bg-black ">
+				<Carousel
+					leftControl="L"
+					rightControl="R"
+					slideInterval={5000}
+					slide=""
+					className=""
+				>
+					<div className=" pt-[10%] text-center text-white">
+						<div className=" text-center text-5xl font-bold"> Kickstart</div>
+						<div className=" mb-[10%] text-center text-5xl italic">
+							{" "}
+							Your learning
+						</div>
+						<button className="rounded-xl bg-purple-700 px-7 py-2 text-lg font-bold">
+							Kickstart
+						</button>
+					</div>
+					<div className=" text-center text-white">
+						<div className=" text-5xl italic text-slate-300">
+							{" "}
+							Connect with Experts and boost your learning
+						</div>
+						<img
+							className=" mx-auto rounded-2xl"
+							src="https://source.unsplash.com/800x400/?videocall"
+							alt=""
+						/>
+						<div className=" text-xl text-slate-500">
+							{" "}
+							Join live sessions with mentors and clear your doubt.{" "}
+						</div>
+					</div>
+				</Carousel>
+			</div>
+		</>
+	);
+};
+export default LandingPage;
