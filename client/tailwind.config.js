4dc3fe9fcf436ae989ebf51d0fb4e8ab657d11d6
Yuvi <81169447+YUVRAJg37@users.noreply.github.com>
2022-09-18 07:26:55 +0530

    added bg

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,13 +1,14 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        background: "url('./images/LandingPage.png')",
+      },
+    },
   },
-  plugins: [
-    require('flowbite/plugin'),
-    require('@tailwindcss/forms'),
-  ],
-}
+  plugins: [require("flowbite/plugin"), require("@tailwindcss/forms")],
+};
